4bf3e7a441796c2488ac9d719054199d15fc092d
nitecifer <nitegupta2004@gmail.com>
2025-02-11 02:10:21 +0530

    DataTypes

---
+++ 02/03/script.js
@@ -0,0 +1,46 @@
+// Data Types
+
+//Numbers 
+let num = 3.65;
+let num1 = 20;
+console.log(num + num1)
+// String 
+let FirstName = 'Nitish';
+const LastName = 'Gupta';
+
+console.log(FirstName + ' ' + LastName )
+
+
+// Booleans 
+let lastLoginDate = null;
+
+console.log(lastLoginDate)
+
+
+// object 
+
+const person = {
+    FirstName:'Nitish',
+    LastName: 'Gupta',
+    age: 21,
+    isloggedIn: false
+
+}
+console.log(person)
+
+
+// JS 1 -> true || 0 -> false 
+
+// JS true | false 
+
+// true -> 1
+// fale -> 0
+
+
+
+// string '1' + '1' = string
+// string + number = string 
+// string * number = number 
+
+
+// typeof operator
--- 02/script.js
+++ 02/script.js
@@ -1,13 +1,36 @@
 //Variables
 
+// var || let || const
+
+// var -> global
+//Re-assign
+//kahi bhi access kr lo 
+
 
 var height = 20;
 
 console.log(height);
+if(true){
+    a = 39;
+}
+console.log(a)
+
+// let - Local
+//Re-assign
+// Access sirf khud ke block me hota hai 
 
 
 let age = 30;
  console.log(age);
+
+ if(true){
+    b = 7;
+    console.log(b)
+ }
+
+// const - Local
+// Re-assign nhi kr sakte hai
+// Access sirf apne block me hota hai
 
 
  const width = 40;
