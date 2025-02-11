c2ffa80cc661b622e1c66f396e3340656beca2d7
nitecifer <nitegupta2004@gmail.com>
2025-02-11 18:43:54 +0530

    conditional

---
+++ 02/03/04/script.js
@@ -0,0 +1,17 @@
+// conditional statement 
+
+// const age = 80
+
+// if (age >= 80){
+//     console.log('Yes, you are an adult')
+// }
+// else if(age >= 18){
+//     console.log("Yes , you can vote")
+// }
+
+// else{
+//     console.log('No , you cant vote' )
+// }
+// // Teranry Operator
+// const a = 20
+// a >=80 ? console.log('yes') : console.log('No')
---
+++ 02/03/04/script1.js
@@ -0,0 +1,15 @@
+let a = 30
+let opt = '/'
+let b = 40
+
+switch(opt){
+    case '+': console.log(a + b)
+    break
+    case '-': console.log(b - a)
+    break
+    case '*': console.log(a * b)
+    break
+    case '/': console.log(b/a)
+    break
+    default: console.log('IDK')
+}
