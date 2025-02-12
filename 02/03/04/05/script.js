0835bcd93fb83919d8822ff820421e7670be3c1d
nitecifer <nitegupta2004@gmail.com>
2025-02-12 22:19:06 +0530

    Logical Operator

---
+++ 02/03/04/05/script.js
@@ -0,0 +1,28 @@
+// Logical Operators
+// AND -> All cond. must be true (&&)
+
+
+// const age = 20
+// const gender = 'male'
+
+// if(age >= 18 && gender == 'male' ){
+//     console.log('you are an Adult')
+// }
+
+
+// OR  -> Atleast one cond. should be true (||)
+
+// const age = 20
+// const gender ='male'
+
+// if(age >= 18 || gender == 'male '){
+//     console.log('you are an Adult')
+// }
+
+// NOT -> !
+
+const number = 18;
+
+if(number % 2 != 0){
+    console.log('Odd')
+}
