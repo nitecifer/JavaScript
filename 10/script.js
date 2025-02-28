599d80ecea58c08da425291c662564f1fe282e7a
nitecifer <nitegupta2004@gmail.com>
2025-02-28 16:04:23 +0530

    Simple Array

---
+++ 10/script.js
@@ -0,0 +1,10 @@
+//Arrays
+
+
+const students = ["Nitish", "Nikhilesh", "Aman","Sahil"]
+
+students[2] = "Vinay"
+students.push("Ritesh")
+students.reverse(students)
+students.pop(3)
+console.log(students)
